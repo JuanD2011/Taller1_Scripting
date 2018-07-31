b374ff8e6a6adcd18c808e5724c945094f0e8d4d
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-07-30 21:25:15 -0500

    CanvasController

---
+++ Taller1_Tienda/Assets/Scripts/CanvasController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour {
+
+    [SerializeField] GameObject canvasShop, canvasInventory;
+
+    private void Start()
+    {
+        canvasShop.SetActive(false);
+        canvasInventory.SetActive(false);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            canvasShop.SetActive(true);
+            canvasInventory.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.I)) {
+            canvasInventory.SetActive(true);
+            canvasShop.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (canvasShop.activeInHierarchy || canvasInventory.activeInHierarchy) {
+                canvasShop.SetActive(false);
+                canvasInventory.SetActive(false);
+            }
+        }
+	}
+}
